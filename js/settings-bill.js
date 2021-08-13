2907d6251415b03a0c2089f539a5c8c6fd24d3bd
Mohau <mohaumodisana@gmail.com>
2021-08-13 16:11:32 +0200

    adding stuff

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,53 +1,41 @@
-// get a reference to the sms or call radio buttons
-var RadioBtns = document.querySelector(".billItemTypeWithSettings:checked");
-    if (RadioBtns){
-        var billItemType3 = RadioBtns.value
-        // billItemType will be 'call' or 'sms'
-    }
+
 const callTotalSettings = document.querySelector(".callTotalSettings");
 const smsTotalSettings = document.querySelector(".smsTotalSettings");
 const totalSettings = document.querySelector(".totalSettings");
 
-// get refences to all the settings fields
+//settings
 const callCostSetting = document.querySelector(".callCostSetting");
 const smsCostSetting = document.querySelector(".smsCostSetting");
 const warningLevelSetting = document.querySelector(".warningLevelSetting");
 const criticalLevelSetting = document.querySelector(".criticalLevelSetting");
 
-//get a reference to the add button
 const AddBtn = document.querySelector(".button-primary");
 
-//get a reference to the 'Update settings' button
+
 const updateSettingsbtn = document.querySelector(".updateSettings");
 
-// create a variables that will keep track of all the settings
+//values that has been set
 var callCost=callCostSetting.value;
 var smsCost=smsCostSetting.value;
 var warning=warningLevelSetting.value;
 var critical=criticalLevelSetting.value;
 
-// create a variables that will keep track of all three totals.
+//initializing
 var call_total=0;
 var sms_total=0;
 var F_total=0;
 
-//add an event listener for when the 'Update settings' button is pressed
 updateSettingsbtn.addEventListener("click", function(){
 
-//add an event listener for when the add button is pressed
 AddBtn.addEventListener("click", function(){
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the call / sms total
-// * add the appropriate value to the overall total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right color.
+//converting the radio buttons
+ var RadioBtns = document.querySelector(".billItemTypeWithSettings:checked");
+    if (RadioBtns){
+        var billItemType3 = RadioBtns.value
+    }
 
 function BillTotal(){
-    // get the value entered in the billType textfield
-
-    // update the correct total
+    
     if (billItemType3 === "call"){
         callsTotal += callCost
     }
@@ -55,15 +43,12 @@ function BillTotal(){
         smsTotal += smsCost;
     }
     
-    //update the totals that is displayed on the screen.
     callTotalSettings.innerHTML = callsTotal.toFixed(2);
     smsTotalSettings.innerHTML = smsTotal.toFixed(2);
     F_total = callsTotal + smsTotal;
     totalSettings.innerHTML = totalCost.toFixed(2);
     
-    //color the total based on the criteria
     if (totalCost >= warning){
-        // adding the danger class will make the text red
         totalSettings.classList.add("danger");
     }
     else if (totalCost >= critical){
@@ -75,4 +60,5 @@ function BillTotal(){
 AddBtn.addEventListener('click', BillTotal);
 
 });
+
 });
